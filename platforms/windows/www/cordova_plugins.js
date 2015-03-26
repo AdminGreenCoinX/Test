84099aa51206b9505b961d5af66b7686c5c0684a
Nilam Doctor <nilamdoc@gmail.com>
2015-03-26 23:55:47 +0530

    testing ip

---
+++ platforms/windows/www/cordova_plugins.js
@@ -0,0 +1,17 @@
+﻿cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.albahra.plugin.networkinterface/www/networkinterface.js",
+        "id": "com.albahra.plugin.networkinterface.networkinterface",
+        "clobbers": [
+            "window.networkinterface"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.albahra.plugin.networkinterface": "1.0.7"
+}
+// BOTTOM OF METADATA
+});
