eb4c0e3da7bd3613cfd5146552dd97042f69bd0a
Nilam Doctor <nilamdoc@gmail.com>
2015-04-07 07:09:00 +0530

    index

--- www/js/index.js
+++ www/js/index.js
@@ -13,7 +13,7 @@ $(document).on("pageshow", function () {
         var options = new ContactFindOptions();
         options.filter = "";
         options.multiple = true;
-        var filter = ["displayName", "phoneNumbers"];
+        var filter = ["displayName", "phoneNumbers","email"];
         navigator.contacts.find(filter, onSuccess, onError, options);
     } else if ($("#addContact").length == 1) {
         bindAddContactEvents();
@@ -36,6 +36,7 @@ function onSuccess(contacts) {
                 }
                 html += "</ul>";
             }
+												html += '<h3>'+contacts[i].email+'</h3>';
             html += '</li>';
         }
     }
